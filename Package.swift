a4af834e8966d614bc12ce73fcb404d8c8cb3b0b
meeranoomeera <107923488+meeranoomeera@users.noreply.github.com>
2024-09-04 14:12:16 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "Markdown",
     platforms: [
         .iOS(.v14),
-        .macOS(.v11)
+        .macOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
